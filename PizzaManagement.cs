f42fe456e091763a39381d094e1b90f71ed9dc69
BhavanaRamyaBanala <ramyabanala12@gmail.com>
2024-02-24 18:31:05 +0530

    First Commit

---
+++ PizzaManagement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsAutomateapp
+{
+    public class PizzaManagement
+    {
+        static List<Pizza> pizzas = new List<Pizza>() {
+            new Pizza { Id = 1,Name="Paneer pizza",Price=650},
+            new Pizza { Id = 2,Name="Mushroom pizza",Price=550},
+            new Pizza { Id = 3,Name="Chicken pizza",Price=850},
+        };
+        public  List<Pizza> AllPizzas()
+        {
+            return pizzas;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using JenkinsAutomateapp;
+using System.Security;
+
+Console.WriteLine("Joe's Pizza details!!");
+Console.WriteLine("---------------------");
+PizzaManagement pm = new PizzaManagement();
+foreach (var p in pm.AllPizzas())
+{
+    Console.WriteLine("Id: "+p.Id);
+    Console.WriteLine("Name: "+p.Name);
+    Console.WriteLine("Price: "+p.Price);
+    Console.WriteLine("---------------------");
+}
